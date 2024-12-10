14a3f353f903546f10d0519ab696fe61b1efa198
Jhon Francis Duarte <jhamaicaduarte@gmail.com>
2024-12-10 20:23:40 +0800

    add

--- resources/views/livewire/admin-safety-and-health-reports-table.blade.php
+++ resources/views/livewire/admin-safety-and-health-reports-table.blade.php
@@ -1,27 +1,29 @@
-<div class="w-full flex justify-center">
+<div class="w-full flex justify-center text-sm">
     <div class="w-full bg-white dark:bg-gray-800 rounded-2xl p-6 shadow-md">
         <h1 class="text-lg font-bold text-center text-black dark:text-white mb-6">
             MGB CALABARZON CUMULATIVE SAFETY AND HEALTH REPORT {{ $selectedYear }}
         </h1>
-        
+
         <div class="mb-4 flex flex-col sm:flex-row justify-between items-end">
             <!-- Search Mine Operators -->
             <div class="w-full sm:w-1/2 mb-4 sm:mb-0 sm:mr-4">
-                <input wire:model.live="search" type="text" id="search" placeholder="Search mine operator name" class="w-full px-4 py-2 border rounded-lg text-gray-700 dark:bg-gray-700 dark:text-gray-300" />
+                <input wire:model.live="search" type="text" id="search" placeholder="Search mine operator name"
+                    class="w-full px-4 py-2 border rounded-lg text-gray-700 dark:bg-gray-700 dark:text-gray-300" />
             </div>
-            
+
             <!-- Year Selection -->
             <div class="w-full sm:w-1/3">
-                <select wire:model.live="selectedYear" id="year-select" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500">
+                <select wire:model.live="selectedYear" id="year-select"
+                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500">
                     @foreach($availableYears as $year)
-                        <option value="{{ $year }}">{{ $year }}</option>
+                    <option value="{{ $year }}">{{ $year }}</option>
                     @endforeach
                 </select>
             </div>
         </div>
-        
-        
-        
+
+
+
 
         <!-- Table -->
         <div class="overflow-x-auto">
@@ -45,23 +47,23 @@
                 </thead>
                 <tbody>
                     @foreach ($reports as $mineOperator => $data)
-                        @foreach (['First Quarter', 'Second Quarter', 'Third Quarter', 'Fourth Quarter'] as $quarterName)
-                            <tr class="whitespace-nowrap">
-                                <td class="px-4 py-2 text-center">{{ $mineOperator }}</td>
-                                <td class="px-4 py-2 text-center">{{ $data['Tenement'] }}</td>
-                                <td class="px-4 py-2 text-center">{{ $quarterName }}</td>
-                                <td class="px-4 py-2 text-center">{{ $data[$quarterName]['NLTA'] }}</td>
-                                <td class="px-4 py-2 text-center">{{ $data[$quarterName]['LTA-NF'] }}</td>
-                                <td class="px-4 py-2 text-center">{{ $data[$quarterName]['LTA-F'] }}</td>
-                                <td class="px-4 py-2 text-center">{{ $data[$quarterName]['Days Lost'] }}</td>
-                                <td class="px-4 py-2 text-center">{{ $data[$quarterName]['Manhours Worked'] }}</td>
-                                <td class="px-4 py-2 text-center">{{ $data[$quarterName]['Male Employees'] }}</td>
-                                <td class="px-4 py-2 text-center">{{ $data[$quarterName]['Female Employees'] }}</td>
-                                <td class="px-4 py-2 text-center">{{ $data[$quarterName]['Total Employees'] }}</td>
-                                <td class="px-4 py-2 text-center">{{ $data[$quarterName]['Recorded Diseases'] }}</td>
-                                <td class="px-4 py-2 text-center">{{ $data[$quarterName]['No. of Cases'] }}</td>
-                            </tr>
-                        @endforeach
+                    @foreach (['First Quarter', 'Second Quarter', 'Third Quarter', 'Fourth Quarter'] as $quarterName)
+                    <tr class="whitespace-nowrap">
+                        <td class="px-4 py-2 text-center">{{ $mineOperator }}</td>
+                        <td class="px-4 py-2 text-center">{{ $data['Tenement'] }}</td>
+                        <td class="px-4 py-2 text-center">{{ $quarterName }}</td>
+                        <td class="px-4 py-2 text-center">{{ $data[$quarterName]['NLTA'] }}</td>
+                        <td class="px-4 py-2 text-center">{{ $data[$quarterName]['LTA-NF'] }}</td>
+                        <td class="px-4 py-2 text-center">{{ $data[$quarterName]['LTA-F'] }}</td>
+                        <td class="px-4 py-2 text-center">{{ $data[$quarterName]['Days Lost'] }}</td>
+                        <td class="px-4 py-2 text-center">{{ $data[$quarterName]['Manhours Worked'] }}</td>
+                        <td class="px-4 py-2 text-center">{{ $data[$quarterName]['Male Employees'] }}</td>
+                        <td class="px-4 py-2 text-center">{{ $data[$quarterName]['Female Employees'] }}</td>
+                        <td class="px-4 py-2 text-center">{{ $data[$quarterName]['Total Employees'] }}</td>
+                        <td class="px-4 py-2 text-center">{{ $data[$quarterName]['Recorded Diseases'] }}</td>
+                        <td class="px-4 py-2 text-center">{{ $data[$quarterName]['No. of Cases'] }}</td>
+                    </tr>
+                    @endforeach
                     @endforeach
                 </tbody>
             </table>
